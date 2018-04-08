679aaacbcc3a9f04bb25881514e68c5c3dc3bfa8
Darren Angle <darrenangle@upshotmail.com>
2018-04-08 14:48:54 -0500

    update inspection model

---
+++ src/app/models/inspection.ts
@@ -0,0 +1,22 @@
+export class Inspection {
+    inspection_id: string;
+    dba_name: string;
+    aka_name: string;
+    license_: string;
+    facility_type: string;
+    risk: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+    inspection_date: string;
+    inspection_type: string;
+    results: string;
+    violations: string;
+    latitude: string;
+    longitude: string;
+    location: object;
+    location_city: string;
+    location_address: string;
+    location_zip: string;
+}
