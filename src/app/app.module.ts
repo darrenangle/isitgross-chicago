a0f9850a26f46ef8a75261d611c9febff947a636
Darren Angle <darrenangle@upshotmail.com>
2018-04-08 19:27:57 -0500

    app module file update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,11 +11,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { SearchFiltersComponent } from './components/search-filters/search-filters.component';
 import { SearchQueryNotesComponent } from './components/search-query-notes/search-query-notes.component';
-import { ResultsHeaderComponent } from './components/results-header/results-header.component';
-import { ResultsListComponent } from './components/results-list/results-list.component';
 import { InspectionDetailComponent } from './components/inspection-detail/inspection-detail.component';
 import { AppRouterModule } from './app-router/app-router.module';
 import { FrontPageComponent } from './components/front-page/front-page.component';
+// tslint:disable-next-line
+import { InspectionsSearchResultsListComponent } from './components/inspections-search-results-list/inspections-search-results-list.component';
 
 
 @NgModule({
@@ -24,16 +25,16 @@ import { FrontPageComponent } from './components/front-page/front-page.component
     SearchBarComponent,
     SearchFiltersComponent,
     SearchQueryNotesComponent,
-    ResultsHeaderComponent,
-    ResultsListComponent,
     InspectionDetailComponent,
-    FrontPageComponent
+    FrontPageComponent,
+    InspectionsSearchResultsListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRouterModule
+    AppRouterModule,
+    HttpClientModule
   ],
-  providers: [LoggingService, QueryBuilderService, InspectionDataService],
+  providers: [LoggingService, QueryBuilderService, InspectionDataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
