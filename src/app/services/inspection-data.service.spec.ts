2f8b4a9442000fb6f1fc07d56c8812c892dcb9fc
Darren Angle <darrenangle@upshotmail.com>
2018-04-09 10:40:06 -0500

    service testing

--- src/app/services/inspection-data.service.spec.ts
+++ src/app/services/inspection-data.service.spec.ts
@@ -24,4 +24,20 @@ describe('InspectionDataService', () => {
   it('should be created', inject([InspectionDataService], (service: InspectionDataService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('loadInspections should create 1000 inspections', inject([InspectionDataService], (service: InspectionDataService) => {
+    service.loadInspections();
+    expect(service.inspectionData._value.length == 1000;
+  }));
+  it('inspection by id exists', inject([InspectionDataService], (service: InspectionDataService) => {
+    const inspection = service.getInspectionByInspectionId(579837);
+    expect(inspection).toBeTruthy();
+  }));
+  it('inspection by id data exists and is correct after subscription', inject([InspectionDataService], (service: InspectionDataService) => {
+    const inspectionObs = service.getInspectionByInspectionId(2116562);
+    const inspection = inspectionObs.subscribe( inspection => {
+      expect(inspection.address).toBe('1055 W BRYN MAWR AVE ');
+    });
+
+  }));
 });
